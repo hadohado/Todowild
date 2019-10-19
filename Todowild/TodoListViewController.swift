8eca94eb170b06f2838b6085c9cf1f8a3e012619
hd <ha_do@yahoo.com>
2019-10-19 01:39:49 -0700

    2nd   change Viewcontroller.swift to TodoListViewcontroller.swift    and change class name inside this new file to TodoListViewController ...

--- Todowild/TodoListViewController.swift
+++ Todowild/TodoListViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+// class ViewController: UIViewController {
+class TodoListViewController: UITableViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
